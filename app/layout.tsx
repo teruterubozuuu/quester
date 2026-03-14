ad29a1fadee60c238e1e717c98b7beb0a1f12554
Joylynne Esportuno <degozarunyan@gmail.com>
2026-03-14 19:35:39 +0800

    chore: initialize Quester project

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
@@ -10,6 +11,11 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: "Quester",
+  description: "Your app description",
+}
 
 export default function RootLayout({
   children,
@@ -23,7 +29,9 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   )
